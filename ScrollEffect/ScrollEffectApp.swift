02a540a7af97520c43f9b6e47c356fa94d6e5016
Christian Manzaraz <magnusmanz@gmail.com>
2024-09-17 18:32:59 -0300

    Initial Commit

---
+++ ScrollEffect/ScrollEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollEffectApp.swift
+//  ScrollEffect
+//
+//  Created by Christian Manzaraz on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrollEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
